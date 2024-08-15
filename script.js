a170d3dcdc40de85aac971ce4ee5eaba21a966f4
Ágata Rafaela <163327581+zPookiePie@users.noreply.github.com>
2024-08-15 17:04:25 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,26 @@
+function carregar() {
+    var msg = document.getElementById('msg')
+    var img = document.getElementById('imagem')
+    var msg2 = document.getElementById('msg2')
+    var data = new Date()
+    var hora = data.getHours()
+    var minute = data.getMinutes()
+    msg.innerHTML = `Agora são ${hora} horas e ${minute} minutos`
+    if (hora >=0 && hora < 6){
+        img.src= '/imagens/madrugada.jpg'
+        document.body.style.background ='#25338aff'
+        msg2.innerHTML = `Boa Madrugada!!`
+    } else if (hora >= 6 && hora < 12) {
+        img.src = '/imagens/manha.jpg'
+        document.body.style.background ='#77c0e9ff'
+        msg2.innerHTML = `Bom Dia!!`
+    } else if (hora >= 12 && hora < 18) {
+        img.src = '/imagens/tarde.jpg'
+        document.body.style.background ='#ffc07aff'
+        msg2.innerHTML = `Boa Tarde!!`
+    } else {
+        img.src = '/imagens/noite.jpg'
+        document.body.style.background ='#1a2340ff'
+        msg2.innerHTML = `Boa Noite!!`
+    }
+}
